d98edc2756281d97e422dabecd19215d37e8d7a4
Beomsun <beomsun22@gmail.com>
2022-01-15 12:26:48 +0900

    feat : 저장 로직 추가

--- order-service/src/main/java/com/bs/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/bs/orderservice/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.bs.orderservice.controller;
 
-import com.bs.orderservice.kafka.KafkaProducer;
+import com.bs.orderservice.service.OrderService;
+import com.bs.orderservice.service.kafka.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class OrderController {
     private final KafkaProducer kafkaProducer;
+    private final OrderService orderService;
     @GetMapping("")
     public String test(){
         log.info("order-service");
@@ -18,10 +20,12 @@ public class OrderController {
     }
 
     @PostMapping("products/{id}/order")
-    public String createOrder(@PathVariable String id){
+    public String createOrder(@PathVariable Long id){
         log.info("product_id = {} + 주문 요청", id);
-        kafkaProducer.sendProductIdToProduct(id);
+        orderService.createOrder(id);
+        kafkaProducer.sendProductIdToProduct(String.valueOf(id));
         return id + "주문 성공";
     }
 
 }
+
