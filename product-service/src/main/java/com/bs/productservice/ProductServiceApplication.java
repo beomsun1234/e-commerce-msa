85e9f30cda6959db84696bba6d066675ca71c172
Beomsun <beomsun22@gmail.com>
2022-01-15 12:31:04 +0900

    feat : jpa auditing 설정

--- product-service/src/main/java/com/bs/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/bs/productservice/ProductServiceApplication.java
@@ -3,8 +3,10 @@ package com.bs.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ProductServiceApplication {
 
