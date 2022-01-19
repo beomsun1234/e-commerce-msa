599d2e680a13dd0e7989cf66dac8cd15b62dfd89
Beomsun <beomsun22@gmail.com>
2022-01-19 18:13:04 +0900

    feat : 서비스간 동기식 통신을 위한 FeignClients 설정

--- product-service/src/main/java/com/bs/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/bs/productservice/ProductServiceApplication.java
@@ -3,9 +3,11 @@ package com.bs.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
 public class ProductServiceApplication {
