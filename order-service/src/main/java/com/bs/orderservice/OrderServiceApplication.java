49f7fb4f6e503300f2f1e04a55645957e52c58f3
Beomsun <beomsun22@gmail.com>
2022-01-19 18:11:15 +0900

    feat : 서비스간 동기식 통신을 위해 FeignClients 설정

--- order-service/src/main/java/com/bs/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/bs/orderservice/OrderServiceApplication.java
@@ -3,9 +3,11 @@ package com.bs.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableFeignClients
 @EnableJpaAuditing
 @EnableDiscoveryClient
 @SpringBootApplication
