3c71dc6de1631bbaab2ce4b6bc9eb1845c1bf66d
Beomsun <beomsun22@gmail.com>
2022-01-27 18:06:32 +0900

    feat : @EnableDiscoveryClient 어노테이션 추가

--- product-service/src/main/java/com/bs/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/bs/productservice/ProductServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
